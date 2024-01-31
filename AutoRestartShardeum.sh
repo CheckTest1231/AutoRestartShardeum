11236636500d1b56346359091d1bb95a2a98e7a6
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2024-01-31 18:51:44 +0200

    Update AutoRestartShardeum.sh

--- AutoRestartShardeum.sh
+++ AutoRestartShardeum.sh
@@ -6,41 +6,23 @@ mkdir -p "$HOME/AutoRestartShardeum"
 # Перехід у папку .shardeum
 cd "$HOME/.shardeum" || exit
 
-# Встановлення українського часового поясу для команди date
-export TZ="Europe/Kiev"
+# Виклик команди через shell.sh та запис результатів
+./shell.sh > "$HOME/AutoRestartShardeum/status.txt"
 
-# Виконання команд та запис результатів у файли
-{
-    # Виконання shell.sh
-    ./shell.sh
+# Перевірка результатів та виконання необхідних дій
+status=$(awk '/state:/ {print $2}' "$HOME/AutoRestartShardeum/status.txt")
 
-    # Затримка, щоб дати час завершити виконання ./shell.sh
-    sleep 7
+# Запис дати та часу у форматі українського часу
+datetime=$(date +"[%a %d %b %Y %I:%M:%S %p %Z]")
+
+# Запис статусу у файл LogInfo.txt разом з датою та часом
+echo "$datetime Status: $status" >> "$HOME/AutoRestartShardeum/LogInfo.txt"
+
+# Перевірка, чи значення статусу "stopped", і виконання команди operator-cli start
+if [ "$status" = "stopped" ]; then
+    # Виконання команди operator-cli start
+    ./shell.sh start
     
-    # Виведення відладки
-    echo "Debug: Executing operator-cli status" >&2
-    
-    # Виконання operator-cli status та зчитування значення статусу
-    status=$(operator-cli status | awk '/state:/ {print $2}')
-    
-    # Виведення відладки
-    echo "Debug: Got status: $status" >&2
-    
-    # Затримка для забезпечення додаткового часу на отримання статусу
-    sleep 7
-    
-    # Запис дати та часу у форматі українського часу
-    datetime=$(date +"[%a %d %b %Y %I:%M:%S %p %Z]")
-    
-    # Запис статусу у файл LogInfo.txt разом з датою та часом
-    echo "$datetime Status: $status" >> "$HOME/AutoRestartShardeum/LogInfo.txt"
-    
-    # Перевірка, чи значення статусу "stopped", і виконання команди operator-cli start
-    if [ "$status" = "stopped" ]; then
-        # Виконання команди operator-cli start
-        operator-cli start
-        
-        # Запис виконання команди у файл LogAutoRestart.txt разом з датою та часом
-        echo "$datetime Restarted Sharduem" >> "$HOME/AutoRestartShardeum/LogAutoRestart.txt"
-    fi
-} &>> "$HOME/AutoRestartShardeum/debug.log"  # Перенаправлення стандартного виведення та помилок в файл debug.log
+    # Запис виконання команди у файл LogAutoRestart.txt разом з датою та часом
+    echo "$datetime Restarted Sharduem" >> "$HOME/AutoRestartShardeum/LogAutoRestart.txt"
+fi
