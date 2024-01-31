40012a0e55ddc9bfec22f316d1a6adfae03042fe
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2024-01-31 18:32:41 +0200

    Update AutoRestartShardeum.sh

--- AutoRestartShardeum.sh
+++ AutoRestartShardeum.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
 # Створення папки AutoRestartShardeum, якщо її не існує
-mkdir -p $HOME/AutoRestartShardeum
+mkdir -p "$HOME/AutoRestartShardeum"
 
 # Перехід у папку .shardeum
-cd $HOME/.shardeum
+cd "$HOME/.shardeum" || exit
+
+# Встановлення українського часового поясу для команди date
+export TZ="Europe/Kiev"
 
 # Виконання команд та запис результатів у файли
 {
     # Виконання shell.sh
     ./shell.sh
+
+    # Затримка, щоб дати час завершити виконання ./shell.sh
+    sleep 7
     
-    # Затримка 5 секунд перед отриманням статусу
-    sleep 5
+    # Виконання operator-cli status та зчитування значення статусу
+    status=$(operator-cli status | awk '/state:/ {print $2}')
     
-    # Виконання operator-cli status та зчитування значення "state"
-    status=$(operator-cli status | grep "state:" | awk '{print $2}')
+    # Затримка для забезпечення додаткового часу на отримання статусу
+    sleep 7
     
-    # Запис дати та часу
-    datetime=$(date)
+    # Запис дати та часу у форматі українського часу
+    datetime=$(date +"[%a %d %b %Y %I:%M:%S %p %Z]")
     
     # Запис статусу у файл LogInfo.txt разом з датою та часом
-    echo "[$datetime] Status: $status" >> $HOME/AutoRestartShardeum/LogInfo.txt
+    echo "$datetime Status: $status" >> "$HOME/AutoRestartShardeum/LogInfo.txt"
     
     # Перевірка, чи значення статусу "stopped", і виконання команди operator-cli start
     if [ "$status" = "stopped" ]; then
@@ -29,6 +35,6 @@ cd $HOME/.shardeum
         operator-cli start
         
         # Запис виконання команди у файл LogAutoRestart.txt разом з датою та часом
-        echo "[$datetime] Restarted Sharduem" >> $HOME/AutoRestartShardeum/LogAutoRestart.txt
+        echo "$datetime Restarted Sharduem" >> "$HOME/AutoRestartShardeum/LogAutoRestart.txt"
     fi
 } &> /dev/null
