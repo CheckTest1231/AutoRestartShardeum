04958ff51e704bff333385abecae91be6a1d25db
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2024-01-31 19:38:41 +0200

    Update AutoRestartShardeum.sh

--- AutoRestartShardeum.sh
+++ AutoRestartShardeum.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
 
-# Перевірка, чи існує папка AutoRestartShardeum, і створення її, якщо потрібно
-mkdir -p "$HOME/AutoRestartShardeum"
+# Шляхи до файлів журналів
+LOG_INFO="$HOME/AutoRestartShardeum/LogInfo.txt"
+LOG_RESTART="$HOME/AutoRestartShardeum/LogRestart.txt"
 
-# Перехід у папку .shardeum
-cd "$HOME/.shardeum" || exit
-
-# Виклик shell.sh
-./shell.sh
-
-# Безкінечний цикл для постійного виконання команди operator-cli status
-while true; do
-    # Отримання статусу через operator-cli
-    status=$(operator-cli status | grep "state:" | awk '{print $2}')
-
-    # Запис дати та часу
-    datetime=$(date +"[%a %d %b %Y %I:%M:%S %p %Z]")
-
-    # Запис статусу у файл LogInfo.txt разом з датою та часом
-    echo "$datetime Status: $status" >> "$HOME/AutoRestartShardeum/LogInfo.txt"
-
-    # Перевірка, чи статус "stopped", і виклик команди operator-cli start
-    if [ "$status" = "stopped" ]; then
-        # Виклик команди operator-cli start
-        operator-cli start
-
-        # Запис виклику команди у файл LogAutoRestart.txt разом з датою та часом
-        echo "$datetime Restarted Sharduem" >> "$HOME/AutoRestartShardeum/LogAutoRestart.txt"
+# Функція для виконання команди та запису результату в файл
+execute_and_log() {
+    # Виконати команду та отримати її результат
+    result=$(docker exec shardeum-dashboard operator-cli status | grep -oP 'state:\s*\K\w+')
+    
+    # Записати результат у файл разом з українським часом
+    datetime=$(date "+[%a %d %b %Y %H:%M:%S %Z]")
+    echo "$datetime Status: $result" >> "$LOG_INFO"
+    
+    # Перевірити, чи значення state: stopped, і якщо так, виконати команду для запуску
+    if [ "$result" = "stopped" ]; then
+        echo "$datetime Sharduem was stopped, restarting..." >> "$LOG_INFO"
+        echo "$datetime Restarted Sharduem" >> "$LOG_RESTART"
+        docker exec shardeum-dashboard operator-cli start
     fi
+}
 
-    # Очікування 1 хвилини перед наступною ітерацією циклу
-    sleep 60
-done
+# Викликати функцію execute_and_log
+execute_and_log
