b2c72cf925fb87f25d1e0d08e3d72188eab6b2c9
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2024-01-31 20:08:14 +0200

    Update AutoRestartShardeum.sh

--- AutoRestartShardeum.sh
+++ AutoRestartShardeum.sh
@@ -4,26 +4,19 @@
 LOG_INFO="$HOME/AutoRestartShardeum/LogInfo.txt"
 LOG_RESTART="$HOME/AutoRestartShardeum/LogRestart.txt"
 
-# Функція для виконання команди та запису результату в файл
-execute_and_log() {
-    # Виконати команду та отримати її результат
-    result=$(docker exec shardeum-dashboard operator-cli status | grep -oP 'state:\s*\K\w+')
-    
-    # Перевірити, чи є результат виводу команди
-    if [ -n "$result" ]; then
-        # Записати результат у файл разом з українським часом
-        datetime=$(date "+[%a %d %b %Y %H:%M:%S %Z]")
-        echo "$datetime Status: $result" >> "$LOG_INFO"
-        
-        # Перевірити, чи значення state: stopped, і якщо так, виконати команду для запуску
-        if [ "$result" = "stopped" ]; then
-            echo "$datetime Sharduem was stopped, restarting..." >> "$LOG_INFO"
-            echo "$datetime Restarted Sharduem" >> "$LOG_RESTART"
-            docker exec shardeum-dashboard operator-cli start
-        fi
+# Виконання команди та запис результату в файл
+result=$(docker exec shardeum-dashboard operator-cli status | grep -oP 'state:\s*\K\w+')
+
+# Перевірити, чи є результат виводу команди
+if [ -n "$result" ]; then
+    # Записати результат у файл разом з українським часом
+    datetime=$(date "+[%a %d %b %Y %H:%M:%S %Z]")
+    echo "$datetime Status: $result" >> "$LOG_INFO"
+
+    # Перевірити, чи значення state: stopped, і якщо так, виконати команду для запуску
+    if [ "$result" = "stopped" ]; then
+        echo "$datetime Sharduem was stopped, restarting..." >> "$LOG_INFO"
+        echo "$datetime Restarted Sharduem" >> "$LOG_RESTART"
+        docker exec shardeum-dashboard operator-cli start
     fi
-}
-
-# Викликати функцію execute_and_log та зробити запис раз на кожну хвилину
-execute_and_log
-
+fi
