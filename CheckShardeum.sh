2d72ff378db82ddc6186d02fb18b44461b60925e
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2024-01-31 22:15:04 +0200

    Update CheckShardeum.sh

--- CheckShardeum.sh
+++ CheckShardeum.sh
@@ -3,7 +3,7 @@
 function printGreen {
   echo -e "\e[1m\e[32m${1}\e[0m"
 }
-
+clear
 echo ""
 printGreen "Останні 36 записи(3год роботи) записи файлу LogInfo.txt"
 tail -n 36 $HOME/AutoRestartShardeum/LogInfo.txt
