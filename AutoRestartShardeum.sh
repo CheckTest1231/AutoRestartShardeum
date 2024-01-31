99788d6490814e82dce26d586b70f9d9b37450dc
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2024-01-31 19:27:54 +0200

    Update AutoRestartShardeum.sh

--- AutoRestartShardeum.sh
+++ AutoRestartShardeum.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-# Встановлення абсолютного шляху до operator-cli
-OPERATOR_CLI="/usr/local/bin/operator-cli"
-
-# Створення папки AutoRestartShardeum, якщо її не існує
+# Перевірка, чи існує папка AutoRestartShardeum, і створення її, якщо потрібно
 mkdir -p "$HOME/AutoRestartShardeum"
 
 # Перехід у папку .shardeum
 cd "$HOME/.shardeum" || exit
 
-# Виклик команди через shell.sh та запис результатів
-./shell.sh > "$HOME/AutoRestartShardeum/status.txt"
+# Виклик shell.sh
+./shell.sh
 
-# Перевірка результатів та виконання необхідних дій
-status=$(awk '/state:/ {print $2}' "$HOME/AutoRestartShardeum/status.txt")
+# Безкінечний цикл для постійного виконання команди operator-cli status
+while true; do
+    # Отримання статусу через operator-cli
+    status=$(operator-cli status | grep "state:" | awk '{print $2}')
 
-# Запис дати, часу та статусу у файл LogInfo.txt
-datetime=$(date +"[%a %d %b %Y %I:%M:%S %p %Z]")
-echo "$datetime Status: $status" >> "$HOME/AutoRestartShardeum/LogInfo.txt"
+    # Запис дати та часу
+    datetime=$(date +"[%a %d %b %Y %I:%M:%S %p %Z]")
 
-# Перевірка, чи значення статусу "stopped", і виконання команди
-if [ "$status" = "stopped" ]; then
-    "$OPERATOR_CLI" start
-    echo "$datetime Restarted Sharduem" >> "$HOME/AutoRestartShardeum/LogAutoRestart.txt"
-fi
+    # Запис статусу у файл LogInfo.txt разом з датою та часом
+    echo "$datetime Status: $status" >> "$HOME/AutoRestartShardeum/LogInfo.txt"
+
+    # Перевірка, чи статус "stopped", і виклик команди operator-cli start
+    if [ "$status" = "stopped" ]; then
+        # Виклик команди operator-cli start
+        operator-cli start
+
+        # Запис виклику команди у файл LogAutoRestart.txt разом з датою та часом
+        echo "$datetime Restarted Sharduem" >> "$HOME/AutoRestartShardeum/LogAutoRestart.txt"
+    fi
+
+    # Очікування 1 хвилини перед наступною ітерацією циклу
+    sleep 60
+done
