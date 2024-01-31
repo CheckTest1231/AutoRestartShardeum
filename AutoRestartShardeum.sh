3d4f43035f71717fed5dde0257606b6d9c8d0f30
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2024-01-31 19:18:03 +0200

    Update AutoRestartShardeum.sh

--- AutoRestartShardeum.sh
+++ AutoRestartShardeum.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Встановлення абсолютного шляху до operator-cli
+OPERATOR_CLI="/usr/local/bin/operator-cli"
+
 # Створення папки AutoRestartShardeum, якщо її не існує
 mkdir -p "$HOME/AutoRestartShardeum"
 
@@ -7,7 +10,7 @@ mkdir -p "$HOME/AutoRestartShardeum"
 cd "$HOME/.shardeum" || exit
 
 # Виклик команди через shell.sh та запис результатів
-sudo /path/to/shell.sh > "$HOME/AutoRestartShardeum/status.txt"
+sudo ./shell.sh > "$HOME/AutoRestartShardeum/status.txt"
 
 # Перевірка результатів та виконання необхідних дій
 status=$(awk '/state:/ {print $2}' "$HOME/AutoRestartShardeum/status.txt")
@@ -18,6 +21,6 @@ echo "$datetime Status: $status" >> "$HOME/AutoRestartShardeum/LogInfo.txt"
 
 # Перевірка, чи значення статусу "stopped", і виконання команди
 if [ "$status" = "stopped" ]; then
-    sudo /path/to/shell.sh start
+    sudo "$OPERATOR_CLI" start
     echo "$datetime Restarted Sharduem" >> "$HOME/AutoRestartShardeum/LogAutoRestart.txt"
 fi
