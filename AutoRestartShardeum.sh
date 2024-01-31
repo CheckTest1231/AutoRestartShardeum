651c25d19a896eb02c065c989f2aa585138e1034
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2024-01-31 20:23:52 +0200

    Update AutoRestartShardeum.sh

--- AutoRestartShardeum.sh
+++ AutoRestartShardeum.sh
@@ -4,28 +4,18 @@
 LOG_INFO="$HOME/AutoRestartShardeum/LogInfo.txt"
 LOG_RESTART="$HOME/AutoRestartShardeum/LogRestart.txt"
 
-# Збереження поточного статусу
-current_status=$(docker exec shardeum-dashboard operator-cli status | grep -oP 'state:\s*\K\w+')
+# Виконання команди та запис результату в файл
+result=$(docker exec shardeum-dashboard operator-cli status | grep -oP 'state:\s*\K\w+')
 
-# Зчитування попереднього статусу з файлу, якщо він існує
-if [ -f "$HOME/AutoRestartShardeum/prev_status.txt" ]; then
-    previous_status=$(cat "$HOME/AutoRestartShardeum/prev_status.txt")
-else
-    previous_status=""
-fi
-
-# Перевірка, чи відбулася зміна статусу
-if [ "$current_status" != "$previous_status" ]; then
-    # Запис статусу в файл
-    echo "$current_status" > "$HOME/AutoRestartShardeum/prev_status.txt"
-    
+# Перевірити, чи є результат виводу команди
+if [ -n "$result" ]; then
     # Записати результат у файл разом з українським часом
     datetime=$(date "+[%a %d %b %Y %H:%M:%S %Z]")
-    echo "$datetime Status: $current_status" >> "$LOG_INFO"
+    echo "$datetime Status: $result" >> "$LOG_INFO"
 
-    # Перевірка, чи значення state: stopped, і якщо так, виконати команду для запуску
-    if [ "$current_status" = "stopped" ]; then
-        echo "$datetime Sharduem was stopped, restarting..." >> "$LOG_INFO"
+    # Перевірити, чи значення state: stopped, і якщо так, виконати команду для запуску
+    if [ "$result" = "stopped" ]; then
+        echo "$datetime Шардеум зупинений, виконуємо ресрарт..." >> "$LOG_INFO"
         echo "$datetime Restarted Sharduem" >> "$LOG_RESTART"
         docker exec shardeum-dashboard operator-cli start
     fi
